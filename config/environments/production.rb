5d4cbad431cfa53be91088b52664864a2f3d26ef
joaodouglasdantas <joaodouglasdantas02b@gmail.com>
2026-04-19 14:21:42 -0300

    configurando o deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  config.public_file_server.enabled = true
 
   config.assets.compile = false
 
